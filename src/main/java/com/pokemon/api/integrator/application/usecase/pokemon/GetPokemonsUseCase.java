6a36906a7cd3b7304d1668ac676255f932203be7
italo-gouveia <italo_2512@hotmail.com>
2023-01-29 16:45:17 -0300

    Remove unused imports

--- src/main/java/com/pokemon/api/integrator/application/usecase/pokemon/GetPokemonsUseCase.java
+++ src/main/java/com/pokemon/api/integrator/application/usecase/pokemon/GetPokemonsUseCase.java
@@ -1,17 +1,10 @@
 package com.pokemon.api.integrator.application.usecase.pokemon;
 
-import com.pokemon.api.integrator.application.gateway.PokemonGateway;
-import com.pokemon.api.integrator.domain.pokemon.Pokemon;
-import com.pokemon.api.integrator.entrypoint.rest.vo.PokemonResponseVO;
 import com.pokemon.api.integrator.entrypoint.rest.vo.PokemonsResultResponseVO;
 import com.pokemon.api.integrator.infrasctructure.feign.PokemonFeignGateway;
-import com.pokemon.api.integrator.infrasctructure.feign.presentation.PokemonPresentation;
 import com.pokemon.api.integrator.infrasctructure.mapper.pokemon.PokemonMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
